398f6116d443e8285711632d17eeb490b706df29
Luiz Gustavo <lglmoura@gmail.com>
2018-05-21 19:35:33 -0300

    Inicio do Projeto

---
+++ app/src/main/java/br/edu/iff/pooa20181/calculoimc/MainActivity.java
@@ -0,0 +1,13 @@
+package br.edu.iff.pooa20181.calculoimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/br/edu/iff/pooa20181/calculoimc/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package br.edu.iff.pooa20181.calculoimc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
